ed07bf879d38bbf7ee996f4b4ea43d31f58d310f
manak1 <mana23happy@hotmail.co.jp>
2021-09-25 00:42:12 +0900

    feat: 質問バリデーション実装

--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,6 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateQuestionDto {
   @IsNotEmpty()
+  @MinLength(20, { message: '質問は20文字以上で入力してね。' })
+  @MaxLength(300, { message: '質問は300文字以下で入力してね。' })
   message: string
 }
