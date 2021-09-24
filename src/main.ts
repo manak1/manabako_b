f697623fbc5492cf923be3ade5a2f5eebaaddc67
manak1 <mana23happy@hotmail.co.jp>
2021-09-25 03:06:18 +0900

    fix: corsの指定方法を修正

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['https://manabako.vercel.app', 'https://manabako-cms.vercel.app']
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['https://manabako.vercel.app', 'https://manabako-cms.vercel.app']
+    }
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
